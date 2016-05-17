c7c7be784a1c84c37f7331de7a75fc581f1c3e11
ankit kumar <ankitkhadria1@gmail.com>
2016-05-17 15:54:31 +0530

    one to one

--- public/js/chat-ui.js
+++ public/js/chat-ui.js
@@ -1,84 +1,136 @@
-$(function(){
-	var socket = io.connect();
-	// id of user that is being private messaged
-	var userToPM = undefined;
+ $(function() {
+    var socket = io.connect();
+    // id of user that is being private messaged
+    var userToPM = undefined;
 
-	$('#choose-nickname').submit(function(e){
-		e.preventDefault();
-		var nick = $('#nickname').val();
-		socket.emit('choose nickname', nick, function(err){
-			if (err) {
-				$('#nick-error').text(err);
-				$('#nickname').val('');
-			} else {
-				$('#nickname-container').hide();
-				$('#chat-container').show();
-			}
-		});
-	});
+    $('#choose-nickname').ready(function() {
 
-	socket.on('names', function(users) {
-		displayUsers(users);
-	});
+        var nick = $('#nickname').val();
+        var admin = $('#admin').val();
+        
+        socket.emit('choose nickname', nick,admin, function(err) {
+            if (err) {
+                $('#nick-error').text(err);
+                $('#nickname').val('');
+            } else {
+                $('#nickname-container').hide();
+                $('#chat-container').show();
+            }
+        });
+    });
 
-	socket.on('new user', function(user) {
-		displayUsers([user]);
-	});
+    socket.on('names', function(users) {
+        displayUsers(users);
+    });
 
-	function displayUsers(users){
-		var html = '';
-		for (var i = 0; i < users.length; i++) {
-			html += '<div class="user" id="user' + users[i].id + '">' + users[i].nick + '</span>';
-		}
-		$('#users').append(html);
-	    $('.user').click(function(e){
-	    	if (!userToPM) {
-	    		$('#pm-col').show();
-	    	}
-	    	userToPM = $(this).attr('id').substring(4);
-	    	$('#user-to-pm').html('<h2>' + $(this).text() + '</h2>');
-	    });
-	}
+    socket.on('new user', function(user) {
 
-	socket.on('user disconnect', function(id){
-		console.log(id);
-		$('#user'+id).remove();
-	});
+        displayUsers([user]);
+    });
 
-    $('#send-message').submit(function(e){
+    function displayUsers(users) {
+
+        var html = '';
+        var html1='';
+        for (var i = 0; i < users.length; i++) {
+            alert(users[i].is_admin+" , "+users[i].nick);
+            if(users[i].is_admin==1)
+                { html1 += '<div class="admin" id="admin' + users[i].id + '">' + users[i].nick + '</div>';}
+            else
+            {
+                html += '<div class="user" id="user' + users[i].id + '">' + users[i].nick + '</div>';
+
+            }
+
+        }
+        $('#users').append(html);
+        $('#admin').append(html1);
+        $('.user').click(function(e) {
+            if (!userToPM) {
+                $('#pm-col').show();
+            }
+            userToPM = $(this).attr('id').substring(4);
+            $('#user-to-pm').html('<h2>' + $(this).text() + '</h2>');
+        });
+    }
+
+    socket.on('user disconnect', function(id) {
+        console.log(id);
+        $('#user' + id).remove();
+    });
+
+    $('#send-message').submit(function(e) {
         e.preventDefault();
         var msg = $('#new-message').val();
         socket.emit('message', msg);
         $('#new-message').val('');
     });
 
-    socket.on('message', function(data){
-    	displayMsg(data.msg, data.nick)
+    socket.on('message', function(data) {
+        displayMsg(data.msg, data.nick)
     });
 
-    socket.on('load old msgs', function(docs){
-    	for (var i = docs.length-1; i >= 0; i--) {
-    		displayMsg(docs[i].msg, docs[i].nick);
-    	}
-    });
 
-    function displayMsg(msg, nick){
-    	console.log(nick);
-    	document.getElementById('title').innerHTML=nick;
-    	var html = "<span class='msg'><strong>" + nick + ":</strong> " + msg;
-    	$('#chat').append(html);
+/*
+    this function for load old db
+
+
+ */
+
+    $('.user').click(function(e) {
+        e.preventDefault();
+        socket.emit('old-db', userToPM);
+        //old message displaying
+        socket.on('load old msgs', function(docs) {
+            for (var i = docs.length - 1; i >= 0; i--) {
+                displayMsg(docs[i].msg, docs[i].nick);
+            }
+        });
+
+    })
+
+    
+    function displayMsg(msg, nick) {
+        console.log(nick);
+        document.getElementById('title').innerHTML = nick;
+        var html = "<span class='msg'><strong>" + nick + ":</strong> " + msg + "</span>";
+        $('#private-chat').append(html);
     }
 
-    $('#send-pm').submit(function(e){
-    	e.preventDefault();
-    	console.log('usertopm',userToPM);
-    	socket.emit('private message', {msg: $('#new-pm').val(), userToPM: userToPM});
-    	$('#new-pm').val('');
+
+// end of load db
+// 
+// send private message
+    $('#send-pm').submit(function(e) {
+        e.preventDefault();
+        console.log('usertopm', userToPM);
+        socket.emit('private message', {
+            msg: $('#new-pm').val(),
+            userToPM: userToPM
+        });
+        $('#new-pm').val('');
     });
 
-    socket.on('private message', function(data){
-    	var html = "<span class='pMsg'><strong>" + data.from + ":</strong> " + data.msg;
-    	$('#chat').append(html);
+    socket.on('private message', function(data) {
+
+        // console.log('data----',data);
+        var html = '';
+        // $('#pMsg').remove();
+        // var history = data.result;
+        // // console.log('history length------',history.length);
+        // // console.log('history--------',history);
+        // var html;
+        // for(var i=0;i<history.length;i++)
+        // {
+        // 	 html += "<span class='pMsg'>"+history[i].from_email+'::'+history[i].msg+"</span>";
+        // 	 html +="<br>";
+        // }
+        // // var html = "<span class='pMsg'>"+data.result+"</span>"
+       html += "<span class='pMsg'><strong>" + data.from + ":</strong> " + data.msg + "</span>";
+        $('#private-chat').append(html);
+
     });
+
+// end of private message
 
 });
--- server.js
+++ server.js
@@ -1,14 +1,135 @@
 var http = require('http'),
 	express = require('express'),
-	chatServer = require('./lib/chat-server');
+	chatServer = require('./lib/chat-server'),
+	db = require('./db.js'),
+	bodyParser = require('body-parser');
+var _ = require('underscore');
+var chatHistory = require('./routes/chat.js');
 
 var app = express();
+app.use(bodyParser.urlencoded({
+	extended: true
+}));
+/*var admin = [];
+var supplierAdmin = [];*/
+
+app.use(bodyParser({
+	defer: true
+}));
+
+app.set('view engine', 'ejs');
+app.set('views', './views');
+
+
+app.use(bodyParser.json());
 app.use(app.router);
 app.use(express.static(__dirname + '/public'));
 
-var server = http.createServer(app).listen('3000', 'localhost'); 
+var server = http.createServer(app).listen('9000', 'localhost');
 chatServer.listen(server);
 
-app.get('/', function(req, res){
-	res.sendfile(__dirname + '/views/index.html');
+/*sql = ' select email from admin ';
+// sub admin
+db.query(sql, function(err, result) {
+	if (err)
+		console.log('err===while rettriving admin');
+	else {
+		// result.forEach( function(element) {
+		// 	// statements
+		// 	admin.push(element.email);
+		// });
+		_.each(result, function(item) {
+
+
+			admin.push(item.email);
+
+		})
+
+		console.log('result admin email', result);
+	}
 });
+
+// supplier sub admin
+sql = ' select email from user ';
+db.query(sql, function(err, result) {
+	if (err)
+		console.log('err===while rettriving supplier admin');
+	else {
+		// result.forEach( function(element) {
+		// 	// statements
+		// 	admin.push(element.email);
+		// });
+		_.each(result, function(item) {
+
+
+			supplierAdmin.push(item.email);
+
+		})
+
+		console.log('result admin email', result);
+	}
+});*/
+app.get('/', function(req, res) {
+
+	res.render('signin.ejs');
+});
+
+app.post('/signIn', function(req, res) {
+	var email = req.body.email;
+	var pass = req.body.pass;
+	q = 'select * from user where email=?';
+
+	db.query(q, [email], function(err, result) {
+		if (result.length > 0) {
+
+			
+			console.log(result[0].pass);
+			if (result[0].password == pass) {
+				res.render('index1.ejs', {
+					result: result[0]
+				});
+
+			}
+
+		} else {
+			console.log('no user exist');
+			res.redirect('/signup');
+		}
+	})
+
+});
+
+app.get('/signup', function(req, res) {
+
+	res.render('signup.ejs');
+
+})
+
+app.post('/signUp', function(req, res) {
+
+	console.log(req.body);
+	var name = req.body.name;
+	var email = req.body.email;
+	var pass = req.body.pass;
+	q = 'insert into user(name,email,password) value("' + name + '","' + email + '","' + pass + '")';
+	db.query(q);
+
+
+	res.redirect('/');
+});
+
+
+// app.get('/', function(req, res){
+// 	res.sendfile(__dirname + '/views/index.html');
+// });
+
+
+app.post('/showOldChat', chatHistory.showOldChat);
+
+function chat(result) {
+
+	res.render('index1.ejs', {
+		result: JSON.stringify(result)
+	});
+
+}
